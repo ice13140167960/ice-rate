664ee08b3d9c4cc41d98a5c0f864adba5e41f12e
ice <1034939359@qq.com>
2019-05-14 17:53:25 +0800

    first commit

---
+++ VueStar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="vue-star">
+    <span
+      v-for="i in max"
+      @click="currentCount=i"
+      :style="{color:i<=currentCount?selectColor:defaultColor}"
+      class="vue-star-item iconfont icon-Starlarge"></span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "VueStar",
+    props: {
+      defaultColor: {
+        type: String,
+        default: 'gray'
+      },
+      selectColor: {
+        type: String,
+        default: '#FFD566'
+      },
+      max: {
+        type: Number,
+        default: 5
+      },
+      count: {
+        type: Number,
+        default: 0
+      },
+    },
+    data(){
+      return{
+        currentCount:this.count
+      }
+    },
+    watch:{
+      currentCount(){
+        this.$emit('change',this.currentCount);
+      },
+    }
+  }
+</script>
+
+<style scoped>
+  @import "./iconfont/iconfont.css";
+
+</style>
---
+++ index.js
@@ -0,0 +1,9 @@
+import VueStar from './VueStar';
+
+const component={
+  install:function (Vue) {
+    Vue.component('vue-star',VueStar)
+  }
+};
+
+export default component;
